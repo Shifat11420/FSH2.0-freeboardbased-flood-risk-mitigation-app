a5ba99d132a0d27d3081e3954c2199e0659e1b70
Andrew Garcia <agarcia@agcenter.lsu.edu>
2023-06-02 09:38:38 -0500

    Added CORS support

--- restProject/restProject/settings.py
+++ restProject/restProject/settings.py
@@ -25,31 +25,38 @@ SECRET_KEY = 'django-insecure-sm(ts(g1g*(lpkk3=)y12!_74zg-79%685=ajcwf#=9)x*=c+(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "lsuag.ngrok.io"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'restApp.apps.RestappConfig',  
+    'restApp.apps.RestappConfig',
     'django_filters',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
 ]
 
 ROOT_URLCONF = 'restProject.urls'
